c803c4cad436471f4a7560a4d06219610bf5957d
sudhakarkatam <sudhakarkatam777@gmail.com>
2025-11-05 01:32:54 +0530

    vite changed

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,8 @@
 
 interface ImportMetaEnv {
   readonly VITE_WEB3FORMS_ACCESS_KEY: string;
+  readonly VITE_GEMINI_API_KEY: string;
+  readonly VITE_OPENROUTER_API_KEY: string;
 }
 
 interface ImportMeta {
